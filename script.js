db67c3465a542dabc7e7f7cc257610704b541f5d
ya2010okb <ya2010kobb@gmail.com>
2026-03-24 17:55:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -345,3 +345,10 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js').catch(err => console.log('SW failed', err));
     });
 }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(reg => console.log('Service Worker Registered!'))
+            .catch(err => console.log('Service Worker Failed!', err));
+    });
+}
